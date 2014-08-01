d207e7b8e084a5ef62def5507495a4d2c081b3a4
Liu Yang <vrace_studios@hotmail.com>
2014-08-01 20:22:39 +0800

    Draw our spaceship. Demonstrated areas: 1) Load texture 2) Use texture 3) Draw a quad 4) Use vertex struct to make things easier

--- Shape/ViewController.m
+++ Shape/ViewController.m
@@ -8,11 +8,26 @@
 
 #import "ViewController.h"
 
-@interface ViewController () {
+@import GLKit;
+
+// This is our vertex format
+// It keeps us away from raw float array
+typedef struct tagVertex
+{
+    // coordinate of our vertex
+    float x, y;
     
+    // coordinate of our texture
+    // normally we use 'uv' to indicate a texture coordinate
+    // uv has a range of (0,0) to (1,1)
+    float u, v;
+} Vertex;
+
+@interface ViewController () {
 }
+
 @property (strong, nonatomic) EAGLContext *context;
-@property (strong, nonatomic) GLKBaseEffect *effect;
+@property GLKTextureInfo *texture;  // our texture
 
 - (void)setupGL;
 - (void)tearDownGL;
@@ -65,9 +80,34 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (GLKTextureInfo*)textureFromImage:(NSString*)imageName
+{
+    NSError *err;
+    GLKTextureInfo *tex = nil;
+    
+    // Find the path in our app bundle
+    NSString *path = [[NSBundle mainBundle] pathForResource:imageName ofType:@"png"];
+    
+    // Load the texture from file
+    if (path)
+        tex = [GLKTextureLoader textureWithContentsOfFile:path options:nil error:&err];
+    
+    if (!tex)
+        NSLog(@"Can't load texture %@", imageName);
+    
+    return tex;
+}
+
 - (void)setupGL
 {
     [EAGLContext setCurrentContext:self.context];
+    
+    // Enable blend so that we can see through transparent picture
+    glEnable(GL_BLEND);
+    // Set blend function to best representing transparent picture
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    self.texture = [self textureFromImage:@"Spaceship"];
 }
 
 - (void)tearDownGL
@@ -86,21 +126,42 @@
     glClearColor(0.4f, 0.6f, 0.9f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
-    
-    float v[] = {
-        0.0f, 0.5f, 1.0f,       0.0f, 0.0f, 1.0f,
-        -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 1.0f,
-        0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 1.0f,
+    // Our sprite (a quad with a picture)
+    Vertex v[] =
+    {
+        // x        y       u       v
+        
+        // first triangle
+        { -0.5f,    0.5f,   0.0f,   0.0f },     // top left of quad
+        { -0.5f,    -0.5f,  0.0f,   1.0f },     // bottom left of quad
+        { 0.5f,     -0.5f,  1.0f,   1.0f },     // bottom right of quad
+        
+        // second triangle
+        { -0.5f,    0.5f,   0.0f,   0.0f },     // top left of quad
+        { 0.5f,     -0.5f,  1.0f,   1.0f },     // bottom right of quad
+        { 0.5f,     0.5f,   1.0f,   0.0f },     // top right of quad
     };
     
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
-    glVertexPointer(2, GL_FLOAT, sizeof(float) * 6, v);
-    glColorPointer(4, GL_FLOAT, sizeof(float) * 6, v + 2);
+    // Enable texture and bind
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, self.texture.name);
     
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // We have vertex and texture coord in our data
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    
+    // Send the data
+    glVertexPointer(2, GL_FLOAT, sizeof(Vertex), v);
+    glTexCoordPointer(2, GL_FLOAT, sizeof(Vertex), &v[0].u);
+    
+    // And draw
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    // Clean up
     glDisableClientState(GL_VERTEX_ARRAY);
-    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    
+    glDisable(GL_TEXTURE_2D);
 }
 
 @end
